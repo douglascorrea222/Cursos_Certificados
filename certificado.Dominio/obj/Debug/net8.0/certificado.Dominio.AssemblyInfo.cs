029acef639ef4d065c9e0c29a054046e72cdf2fa
Douglas Correa <douglas.correa@BRTUV.LOCAL>
2024-06-24 15:31:42 -0300

    Teste de commit

--- certificado.Dominio/obj/Debug/net8.0/certificado.Dominio.AssemblyInfo.cs
+++ certificado.Dominio/obj/Debug/net8.0/certificado.Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("certificado.Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27990413d4c0c5e845906dd9f18f79701a79acbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("certificado.Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("certificado.Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
